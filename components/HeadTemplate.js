7544203c7a518e5d36e812dd71446461886c2c50
masbaehr <38694044+masbaehr@users.noreply.github.com>
2020-04-10 11:33:17 +0200

    add favicons and webmanifest

--- components/HeadTemplate.js
+++ components/HeadTemplate.js
@@ -7,7 +7,11 @@ const HeadTemplate = props => {
             <title>Hello World from HeadTemplate!</title>
             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             <meta name="description" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam"/>
-            <link rel="shortcut icon" href="favicon.ico"></link>
+            
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+            <link rel="manifest" href="/site.webmanifest"></link>
           />
         </Head>
     );
